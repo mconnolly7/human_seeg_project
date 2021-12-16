d6efb5db2fcac3eddff383ebb7f3a48f1f50bd37
Mark <mark.connolly@emory.edu>
2021-12-16 11:02:35 -0500

    Added PS for ADMES001

--- trial_run/experiment_code/parameter_search/run_asynchronous_bipolar_4_program_ps.m
+++ trial_run/experiment_code/parameter_search/run_asynchronous_bipolar_4_program_ps.m
@@ -1,2 +1,13 @@
-AMPLITUDE = 1100;
-ps_helper(@asynchronous_bipolar_4_program, AMPLITUDE)
+a1 = 0.1:.1:.8; f1 = 7 * ones(size(a1));
+a2 = 0.1:.1:.6; f2 = 50 * ones(size(a2));
+a3 = 0.1:.1:.4; f3 = 90 * ones(size(a3));
+a4 = 0.1:.1:.5; f4 = 125 * ones(size(a4));
+
+stimulation_parameters      = [a1 a2 a3 a4; f1 f2 f3 f4];
+stimulation_parameters      = combvec(stimulation_parameters, [100 300 450])';
+
+rng(0)
+rand_idx                 	= randperm(size(stimulation_parameters,1));
+stimulation_parameters      = stimulation_parameters(rand_idx,:);  
+
+ps_helper(@asynchronous_bipolar_4_program, stimulation_parameters)
--- trial_run/helper_methods/ps_helper.m
+++ trial_run/helper_methods/ps_helper.m
@@ -1,35 +1,37 @@
-function  ps_helper(waveform_function, amplitude)
+function  ps_helper(waveform_function, stimulation_parameters)
 
 stimulation_table   = [];
 
-[data_file_name, data_file_path]  = uigetfile('S:\ADMES\*');
+[data_file_name, data_file_path]  = uigetfile('C:\Users\User\Documents\BLACKROCK_LOCAL_FILE_STORAGE\*');
 log_path            = [data_file_path data_file_name(1:end-4) '_stimulation_table.mat'];
 
 if isempty(data_file_name)
     return;
 end
 
-param_f             = [ 7 12 31 62.5 72];
+% param_f             = [ 7 12 31 62.5 72];
+% 
+% n_reps              = 10;
+% param_f_rand        = [];
+% rng(0)
 
-n_reps              = 10;
-param_f_rand        = [];
-rng(0)
-
-for c1 = 1:n_reps
-    rand_idx        = randperm(size(param_f,2));
-    param_f_rand    = [param_f_rand param_f(rand_idx)];   
-end
+% for c1 = 1:n_reps
+%     rand_idx        = randperm(size(param_f,2));
+%     param_f_rand    = [param_f_rand param_f(rand_idx)];   
+% end
 
 train_interval_on   = 5;
-train_interval_off  = 15;
-duration            = 20;
+train_interval_off  = 5;
+duration            = 10;
 
-for c1 = 1:size(param_f_rand,2)
+for c1 = 1:size(stimulation_parameters,2)
     fprintf('Sample: %d\n', c1)
+   
+    amplitude           = stimulation_parameters(c1,1);
+    asynch_frequency    = stimulation_parameters(c1,2);
+    pulse_width         = stimulation_parameters(c1,3);
     
-    asynch_frequency    = param_f_rand(c1);
-    
-    stimulation_row     = waveform_function(train_interval_on, train_interval_off, amplitude, duration, asynch_frequency);
+    stimulation_row     = waveform_function(train_interval_on, train_interval_off, amplitude, duration, asynch_frequency, pulse_width);
     
     stimulation_table   = [stimulation_table; stimulation_row];
     
